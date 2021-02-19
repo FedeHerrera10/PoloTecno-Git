a708a4b60819e3c9e5d2852181b6455c823e2d61
FedeHerrera10 <herrera3299@gmail.com>
2021-02-19 11:52:50 -0300

    Subiendo proyecto terminado random nickname

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # PoloTecno-Git
+
 Repositorio del modulo de Git - PoloTecno UNLaR
+
+# Descripcion
+
+Actividad numero 1 el curso Git . Pequeño proyecto que crea de manera aletoria un nickname, utilizando html,css,js.
+
+### Instalación 🔧
+
+Solo es necesario contar con un servidor de sitios web local (apache,gnix, etc).
+Si utilizas Visual Studio Code, se recomienda instalar la extension Live Server,
+y de esa manera no es necesario contar con un servidor.
+
+## Licencia 📄
+
+Este repositorio es de uso publico.
+
+## 🎁
+
+⌨️ creado por [Fede Herrera](https://github.com/FedeHerrera10) 😊
---
+++ app.js
@@ -0,0 +1,10 @@
+const $button = document.getElementById('button-nickname');
+const $nickname = document.getElementById('nickname');
+const names = ["Alexei", "Alexx", "Alexzander", "Alf", "Alfee", "Alfie", "Alfred", "Alfy", "Alhaji", "Al-Hassan", "Ali", "Aliekber", "Alieu", "Alihaider", "Alisdair", "Alishan", "Alistair", "Alistar", "Alister", "Aliyaan", "Allan", "Allan-Laiton", "Allen", "Allesandro", "Allister", "Ally", "Alphonse", "Altyiab", "Alum", "Alvern", "Alvin", "Alyas", "Amaan", "Aman", "Amani", "Ambanimoh", "Ameer", "Amgad", "Ami", "Amin", "Amir", "Ammaar", "Ammar", "Callan", "Callan-Adam", "Calley", "Callie", "Callin", "Callum", "Callun", "Callyn", "Calum", "Calum-James", "Calvin", "Cambell", "Camerin", "Cameron", "Campbel", "Campbell", "Camron", "Caolain", "Caolan", "Carl", "Carlo", "Carlos", "Carrich", "Carrick", "Carson", "Carter", "Carwyn", "Casey", "Casper", "Cassy", "Cathal", "Cator", "Cavan", "Cayden", "Cayden-Robert", "Cayden-Tiamo", "Ceejay", "Ceilan", "Ceiran", "Ceirin", "Ceiron", "Cejay", "Celik", "Cephas", "Cesar", "Cesare", "Chad", "Chaitanya", "Chang-Ha", "Charles", "Charley", "Charlie", "Charly", "Chase", "Che", "Chester", "Chevy", "Chi", "Chibudom", "Chidera", "Chimsom", "Chin", "Chintu", "Chiqal", "Chiron", "Chris", "Chris-Daniel", "Chrismedi", "Christian", "Christie", "Christoph", "Christopher", "Christopher-Lee", "Christy", "Chu", "Chukwuemeka", "Cian", "Ciann", "Ciar", "Ciaran", "Ciarian", "Cieran", "Cillian", "Cillin", "Cinar", "CJ", "C-Jay", "Clark", "Clarke", "Clayton", "Clement", "Clifford", "Clyde", "Cobain", "Coban"];
+
+
+$button.addEventListener("click", () => {
+    const index=Math.floor(Math.random() * 130);
+    let nickname = `@${names[index]}${index}`;
+    $nickname.innerText =  nickname;
+})
